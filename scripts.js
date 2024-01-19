bfa15162e4c030bc19880ed2efcf567d93ca1c44
bmbm1107 <marczisbalazs@gmail.com>
2024-01-19 12:56:59 +0100

    finalize layout, adds mobile layout

---
+++ scripts.js
@@ -0,0 +1,23 @@
+﻿
+
+
+const fldPass = document.querySelectorAll('input[type="password"]')
+
+for (i of fldPass) {
+
+    i.addEventListener('change', () => {
+        if (fldPass[0].value !== fldPass[1].value) {
+            for (e of fldPass) {
+                e.classList.add('passw-err')
+            }
+        } else {
+            for (e of fldPass) {
+                e.classList.remove('passw-err')
+            }
+        }
+
+    })
+
+
+}
+
